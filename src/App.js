84be5d31f81ddd65621a134caa22ed27a6c81857
Ben Grabowski <gbowski30@gmail.com>
2019-07-12 21:46:42 -0600

    created listArray

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import List from './composition/List';
 import STORE from './store';
 
 function App(props) {
+  const listArray = STORE.lists.map(listObject => {
+    return <List 
+              key={listObject.id} 
+              header={listObject.header} 
+              cards={listObject.cardIds}
+              />
+  });
+  
   return (
     <main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
       </header>
       <div className="App-List">
-        <List header={STORE.lists[0].header} 
+        {/* <List header={STORE.lists[0].header} 
               cards={STORE.allCards}/>
         <List header={STORE.lists[1].header}
                cards={STORE.allCards}/>
         <List header={STORE.lists[2].header}
               cards={STORE.allCards} />
         <List header={STORE.lists[3].header}
-              cards={STORE.allCards} />
-      </div>
+  cards={STORE.allCards} /> */}
+
+      {listArray}
+      </div>  
     </main>
   );
 }
